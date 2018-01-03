121282fe103f06cd376f136275e2e12474ef0ea8
セロラー <celelyer@serora-no-macbook-pro-52153.local>
2018-01-03 12:11:40 +0900

    重力の設定

--- SmartBall/GameScene.swift
+++ SmartBall/GameScene.swift
@@ -21,9 +21,18 @@ class GameScene: SKScene {
         // 背景色を設定
         backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
         
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -9.8 / 6.0)
+        
         setwall()
         setball()
         setpin()
+    }
+    
+    //画面をタップした時に呼ばれる
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        ball.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: 20))
+        
     }
     
     func setwall(){
@@ -34,16 +43,32 @@ class GameScene: SKScene {
         let wallTexture = SKTexture(imageNamed: "Wall")
         wallTexture.filteringMode = SKTextureFilteringMode.linear
         
-        let wall = SKSpriteNode(texture: wallTexture)
+        wall = SKSpriteNode(texture: wallTexture)
         wall.size = CGSize(width: wallTexture.size().width, height: self.frame.size.height * 0.7)
         wall.position = CGPoint(x: self.frame.size.width - ballsize.width - wall.size.width, y: wall.size.height / 2 + ballsize.height + 3.0)
+        wall.name = "wall"
+        
+        //物理演算プロパティ
+        wall.physicsBody = SKPhysicsBody(rectangleOf: wall.size)
+        wall.physicsBody?.isDynamic = false
+        wall.physicsBody?.friction = 0.0    //摩擦係数
+        wall.physicsBody?.restitution = 0.1 //反発係数
         
         let loopTexture = SKTexture(imageNamed: "loop")
         loopTexture.filteringMode = SKTextureFilteringMode.linear
         
-        let loop = SKSpriteNode(texture: loopTexture)
+        loop = SKSpriteNode(texture: loopTexture)
         loop.size = CGSize(width: self.frame.size.width, height: self.frame.size.height / 5.0)
         loop.position = CGPoint(x: self.frame.size.width / 2.0, y: self.frame.size.height - loop.size.height / 2)
+        loop.name = "loop"
+        //物理演算プロパティ
+        loop.physicsBody = SKPhysicsBody(texture: loopTexture, size: loop.size)
+        loop.physicsBody?.isDynamic = false
+        loop.physicsBody?.friction = 0.0    //摩擦係数
+        loop.physicsBody?.restitution = 0.5 //反発係数
+        
+        //画面端を壁にする
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
         
         addChild(loop)
         addChild(wall)
@@ -53,9 +78,17 @@ class GameScene: SKScene {
         let ballTexture = SKTexture(imageNamed: "Ball")
         ballTexture.filteringMode = SKTextureFilteringMode.linear
         
-        let ball = SKSpriteNode(texture: ballTexture)
+        ball = SKSpriteNode(texture: ballTexture)
         ball.size = CGSize(width: ballTexture.size().width * 1.5, height: ballTexture.size().height * 1.5)
-        ball.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 1.5)
+        ball.position = CGPoint(x: self.frame.size.width - ball.size.width / 2, y: self.frame.size.height / 1.5)
+        ball.name = "ball"
+        
+        //物理演算プロパティ
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+        ball.physicsBody?.isDynamic = true
+        ball.physicsBody?.allowsRotation = true //true:回転する
+        ball.physicsBody?.friction = 0.1        //摩擦係数
+        ball.physicsBody?.restitution = 0.1     //反発係数
         
         addChild(ball)
         
@@ -65,8 +98,15 @@ class GameScene: SKScene {
         let pinTexture = SKTexture(imageNamed: "pin")
         pinTexture.filteringMode = SKTextureFilteringMode.linear
         
-        let pin = SKSpriteNode(texture: pinTexture)
-        pin.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2)
+        pin = SKSpriteNode(texture: pinTexture)
+        pin.position = CGPoint(x: ball.size.width / 2, y: self.frame.size.height * 0.8)
+        pin.name = "pin_1"
+        
+        //物理演算プロパティ
+        pin.physicsBody = SKPhysicsBody(circleOfRadius: pin.size.width / 3.0)
+        pin.physicsBody?.isDynamic = false
+        pin.physicsBody?.friction = 0.0    //摩擦係数
+        pin.physicsBody?.restitution = 0.7 //反発係数
         
         addChild(pin)
     }
