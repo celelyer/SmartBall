60a3e7ad608479aabc0d276df5c27c65bf0875bb
セロラー <celelyer@serora-no-macbook-pro-52180.local>
2018-01-05 18:12:25 +0900

    削除判定導入

--- SmartBall/GameScene.swift
+++ SmartBall/GameScene.swift
@@ -19,13 +19,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var ball:SKSpriteNode!
     var pin:SKSpriteNode!
     var curve:SKSpriteNode!
+    var curve2:SKSpriteNode!
+    var cue:SKSpriteNode!
+    var cue_joint:SKNode! //キューのジョイントの支点
     
     var launch:SKSpriteNode!      //発射台
     var out:SKNode!                 //発射台出口
+    var deleteNode:SKSpriteNode!    //ボールが消える判定
+    
+    var tap_point:CGPoint!  //タップした座標
     
     let launchCategory: UInt32 = 1 << 0
     let ballCategory: UInt32 = 1 << 1
     let outCategory: UInt32 = 1 << 2
+    let wallCategory: UInt32 = 1 << 3
+    let deleteCategory: UInt32 = 1 << 4
     
     
     // SKView上にシーンが表示されたときに呼ばれるメソッド
@@ -40,6 +48,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         setball()
         setwall()
         setpin()
+        setcue()
+        setpoint()
         
         //発射台に重力を設定する為のノードを作成
         launch = SKSpriteNode(imageNamed: "Ball")
@@ -73,8 +83,33 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     //画面をタップした時に呼ばれる
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        ball.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: 20))
+        //キューを一度isDynamic = false にして引っ張りやすくする
+        if cue.physicsBody?.isDynamic == true {
+            cue.physicsBody?.isDynamic = false
+        }
+        
+        tap_point = touches.first!.location(in: self)
         setball()
+        
+    }
+    
+    //画面タッチ移動時の呼び出しメソッド
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //タッチした座標を取得する
+        let location = touches.first!.location(in: self)
+        
+        if cue.size.height * 1.3 >= (tap_point.y - location.y) {
+            //スライドに合わせて移動する
+            let action = SKAction.moveTo(y: cue_joint.position.y - (tap_point.y - location.y), duration: 0.0)
+            cue.run(action)
+        }
+        
+    }
+    
+    //タッチを離した時のメソッド
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        cue.physicsBody?.isDynamic = true
+        //cue.physicsBody?.applyForce(CGVector(dx: 0.0, dy: 20.0))
         
     }
     
@@ -102,8 +137,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 
             }
             
+        }else if (contact.bodyA.categoryBitMask & deleteCategory) == deleteCategory || (contact.bodyB.categoryBitMask & deleteCategory) == deleteCategory { //ボールが削除判定に触れた時
+            if (contact.bodyA.categoryBitMask & ballCategory) == ballCategory { //bodyAがボールの時
+                contact.bodyA.node?.removeFromParent()
+            }else{ //bodyBがボールの時
+                contact.bodyB.node?.removeFromParent()
+            }
+            
         }
     }
+
+
     
     func setwall(){
         
@@ -171,6 +215,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         wall4.physicsBody?.isDynamic = false
         wall4.physicsBody?.friction = 0.0    //摩擦係数
         wall4.physicsBody?.restitution = 0.1 //反発係数
+        wall4.physicsBody?.categoryBitMask = wallCategory
         
         let loopTexture = SKTexture(imageNamed: "loop")
         loopTexture.filteringMode = SKTextureFilteringMode.linear
@@ -199,6 +244,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         curve.physicsBody?.friction = 0.0    //摩擦係数
         curve.physicsBody?.restitution = 0.1 //反発係数
         
+        let curve2Texture = SKTexture(imageNamed: "curve2")
+        curve2Texture.filteringMode = SKTextureFilteringMode.linear
+        
+        curve2 = SKSpriteNode(texture: curve2Texture)
+        curve2.size = CGSize(width: ball.size.width * 1.5, height: ball.size.height * 0.5)
+        curve2.position = CGPoint(x: wall.position.x - wall.size.width / 2 - curve2.size.width / 2, y: wall2.position.y - wall2.size.height / 2 - curve2.size.height / 2)
+        curve2.name = "curve"
+        
+        //物理演算プロパティ
+        curve2.physicsBody = SKPhysicsBody(texture: curve2Texture, size: curve2.size)
+        curve2.physicsBody?.isDynamic = false
+        curve2.physicsBody?.friction = 0.0    //摩擦係数
+        curve2.physicsBody?.restitution = 0.1 //反発係数
+        
         //画面端を壁にする
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
         
@@ -208,6 +267,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(wall3)
         addChild(wall4)
         addChild(curve)
+        addChild(curve2)
     }
     
     func setball(){
@@ -224,9 +284,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         ball.physicsBody?.isDynamic = true
         ball.physicsBody?.allowsRotation = true //true:回転する
         ball.physicsBody?.friction = 0.1        //摩擦係数
-        ball.physicsBody?.restitution = 0.1     //反発係数
+        ball.physicsBody?.restitution = 0.3     //反発係数
         ball.physicsBody?.categoryBitMask = ballCategory
-        ball.physicsBody?.collisionBitMask = ballCategory
+        ball.physicsBody?.collisionBitMask = ballCategory | wallCategory
         ball.physicsBody?.fieldBitMask = launchCategory
         
         addChild(ball)
@@ -248,5 +308,75 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         pin.physicsBody?.restitution = 0.7 //反発係数
         
         addChild(pin)
+    }
+    
+    func setcue(){
+        let cueTexture = SKTexture(imageNamed: "cue")
+        cueTexture.filteringMode = SKTextureFilteringMode.linear
+        
+        cue = SKSpriteNode(texture: cueTexture)
+        cue.size = CGSize(width: cueTexture.size().width * 2, height: self.frame.size.height * 0.1)
+        cue.position = CGPoint(x: self.frame.size.width - ball.size.width / 2 - cueTexture.size().width / 2, y: ball.size.width * 3 + wall2.size.height * 1.5)
+        cue.name = "cue"
+        
+        //物理演算プロパティ
+        cue.physicsBody = SKPhysicsBody(texture: cueTexture, size: cue.size)
+        cue.physicsBody?.isDynamic = false
+        cue.physicsBody?.friction = 0.0    //摩擦係数
+        cue.physicsBody?.restitution = 0.5 //反発係数
+        cue.physicsBody?.allowsRotation = false
+        cue.physicsBody?.categoryBitMask = ballCategory
+        cue.physicsBody?.collisionBitMask = ballCategory
+        cue.physicsBody?.mass = (ball.physicsBody?.mass)! * 10
+        
+        let jointTexture = SKTexture(imageNamed: "Ball")
+        jointTexture.filteringMode = SKTextureFilteringMode.nearest
+        
+        cue_joint = SKNode()
+        cue_joint.position = CGPoint(x: self.frame.size.width - ball.size.width / 2 - cueTexture.size().width / 2, y: ball.size.width * 3 + wall2.size.height * 1.5)
+        cue_joint.name = "joint"
+        
+        //物理演算プロパティ
+        cue_joint.physicsBody = SKPhysicsBody(texture: jointTexture, size: jointTexture.size())
+        cue_joint.physicsBody?.isDynamic = false
+        cue_joint.physicsBody?.friction = 0.0    //摩擦係数
+        cue_joint.physicsBody?.restitution = 1.0 //反発係数
+        cue_joint.physicsBody?.categoryBitMask = outCategory
+        
+        //キューを固定するジョイント(バネ)
+        let joint = SKPhysicsJointSpring.joint(withBodyA: cue.physicsBody!, bodyB: cue_joint.physicsBody!, anchorA: cue.position, anchorB: cue_joint.position)
+        
+        //伸縮の周期を設定する
+        joint.frequency = 7.0
+        
+        //伸縮の減衰を設定する
+        joint.damping = 1.0
+        
+        
+        addChild(cue_joint)
+        addChild(cue)
+        self.physicsWorld.add(joint)
+    }
+    
+    //いろんな判定の追加
+    func setpoint(){
+        let deleteTexture = SKTexture(imageNamed: "Wall")
+        deleteTexture.filteringMode = SKTextureFilteringMode.linear
+        
+        deleteNode = SKSpriteNode(texture: deleteTexture)
+        deleteNode.size = wall2.size
+        deleteNode.position = CGPoint(x: self.frame.size.width / 2, y: wall2.position.y + wall2.size.height)
+        deleteNode.name = "delete"
+        
+        //物理演算プロパティ
+        deleteNode.physicsBody = SKPhysicsBody(rectangleOf: wall2.size)
+        deleteNode.physicsBody?.isDynamic = false
+        deleteNode.physicsBody?.friction = 0.0    //摩擦係数
+        deleteNode.physicsBody?.restitution = 0.1 //反発係数
+        deleteNode.physicsBody?.categoryBitMask = deleteCategory
+        deleteNode.physicsBody?.collisionBitMask = ballCategory
+        deleteNode.physicsBody?.contactTestBitMask = ballCategory
+        
+        addChild(deleteNode)
     }
 }
